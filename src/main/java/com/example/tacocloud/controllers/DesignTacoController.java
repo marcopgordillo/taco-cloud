c2ef330e5ba68cc7c5c32e692bbb6db6320bc4bc
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2019-06-26 08:16:57 -0400

    Added OrderController

--- src/main/java/com/example/tacocloud/controllers/DesignTacoController.java
+++ src/main/java/com/example/tacocloud/controllers/DesignTacoController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -42,6 +43,14 @@ public class DesignTacoController {
 
     model.addAttribute("design", new Taco());
     return "design";
+  }
+
+  @PostMapping
+  public String processDesign(Taco design) {
+// Save the taco design...
+// We'll do this in chapter 3
+    log.info("Processing design: " + design);
+    return "redirect:/orders/current";
   }
 
   private List<Ingredient> filterByType(
