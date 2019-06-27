c8db0451b515e4028b7120ad216da0dc42f7d189
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2019-06-27 08:52:05 -0400

    Added authentication JDBC

--- src/main/java/com/example/tacocloud/configuration/SecurityConfig.java
+++ src/main/java/com/example/tacocloud/configuration/SecurityConfig.java
@@ -4,19 +4,38 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  private final DataSource dataSource;
+
+  public SecurityConfig(DataSource dataSource) {
+    this.dataSource = dataSource;
+  }
+
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.inMemoryAuthentication()
-            .withUser("buzz")
-            .password("{noop}infinity")
-            .authorities("ROLE_USER")
-            .and()
-            .withUser("{noop}woody")
-            .password("bullseye")
-            .authorities("ROLE_USER");
+
+    Map<String, PasswordEncoder> encoders = new HashMap<>();
+    encoders.put("bcrypt", new BCryptPasswordEncoder());
+    encoders.put("scrypt", new SCryptPasswordEncoder());
+
+    auth.jdbcAuthentication()
+            .dataSource(dataSource)
+            .usersByUsernameQuery(
+                    "select username, password, enabled from Users where username=?")
+            .authoritiesByUsernameQuery(
+                    "select username, authority from UserAuthorities where username=?")
+            .passwordEncoder(new DelegatingPasswordEncoder("bcrypt", encoders));
   }
 }
