aafc3c4b5355c2187013f1db78813ad7325552f8
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2019-06-27 15:38:24 -0400

    Fixed some RegistrationController

--- src/main/java/com/example/tacocloud/controllers/RegistrationController.java
+++ src/main/java/com/example/tacocloud/controllers/RegistrationController.java
@@ -5,12 +5,9 @@ import com.example.tacocloud.repostory.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -31,11 +28,7 @@ public class RegistrationController {
   }
 
   @PostMapping
-  public String processRegistration(@Valid RegistrationForm form, Errors errors) {
-    if (errors.hasErrors()) {
-      return "registration";
-    }
-
+  public String processRegistration(RegistrationForm form) {
     userRepo.save(form.toUser(passwordEncoder));
     return "redirect:/login";
   }
