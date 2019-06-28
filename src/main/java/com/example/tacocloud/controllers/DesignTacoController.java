3d8eef6eeadce34af7069ce8a8eb3a6e4e1e0b39
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2019-06-28 15:56:38 -0400

    Added put and patch methods

--- src/main/java/com/example/tacocloud/controllers/DesignTacoController.java
+++ src/main/java/com/example/tacocloud/controllers/DesignTacoController.java
@@ -4,6 +4,7 @@ import com.example.tacocloud.configuration.OrderProps;
 import com.example.tacocloud.domain.Taco;
 import com.example.tacocloud.repostory.TacoRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
+
+import static java.util.Objects.isNull;
 
 @Slf4j
 @RestController
@@ -46,5 +49,50 @@ public class DesignTacoController {
   @ResponseStatus(HttpStatus.CREATED)
   public Taco postTaco(@RequestBody Taco taco) {
     return tacoRepo.save(taco);
+  }
+
+  @PutMapping("/{tacoId}")
+  public ResponseEntity<Taco> putTaco(@PathVariable("tacoId") Long tacoId, @RequestBody Taco taco) {
+    Optional<Taco> optTaco = tacoRepo.findById(tacoId);
+
+    if (optTaco.isPresent()) {
+      tacoRepo.save(taco);
+      return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+    }
+    return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+  }
+
+  @PatchMapping(path = "/{tacoId}", consumes = "application/json")
+  public ResponseEntity<Taco> patchTaco(@PathVariable("tacoId") Long tacoId, @RequestBody Taco tacoPatch) {
+    log.info(tacoPatch.toString());
+    Optional<Taco> optTaco = tacoRepo.findById(tacoId);
+    if (optTaco.isPresent()) {
+
+      Taco taco = optTaco.get();
+
+      if (!isNull(tacoPatch.getCreatedAt())) {
+        taco.setCreatedAt(tacoPatch.getCreatedAt());
+      }
+      if (!isNull(tacoPatch.getName())) {
+        taco.setName(tacoPatch.getName());
+      }
+      if (!isNull(tacoPatch.getIngredients()) && !tacoPatch.getIngredients().isEmpty()) {
+        taco.setIngredients(tacoPatch.getIngredients());
+      }
+
+      tacoRepo.save(taco);
+      return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+    }
+    return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+  }
+
+  @DeleteMapping("/{tacoId}")
+  public ResponseEntity deleteTaco(@PathVariable("tacoId") Long tacoId) {
+    try {
+      tacoRepo.deleteById(tacoId);
+      return new ResponseEntity(HttpStatus.NO_CONTENT);
+    } catch (EmptyResultDataAccessException e) {
+      return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
   }
 }
